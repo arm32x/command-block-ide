8807211f4c6b740d9e4ce0e9fa30235b17caa6f3
soyuo07 <166280474+soyuo07@users.noreply.github.com>
2025-07-19 11:23:58 +0900

    Update CommandBlockTypeButton.java

--- src/main/java/arm32x/minecraft/commandblockide/client/gui/button/CommandBlockTypeButton.java
+++ src/main/java/arm32x/minecraft/commandblockide/client/gui/button/CommandBlockTypeButton.java
@@ -2,8 +2,9 @@ package arm32x.minecraft.commandblockide.client.gui.button;
 
 import arm32x.minecraft.commandblockide.client.Dirtyable;
 import arm32x.minecraft.commandblockide.mixin.client.DrawContextAccessor;
-import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.pipeline.RenderPipeline;
 import net.minecraft.block.entity.CommandBlockBlockEntity;
+import net.minecraft.client.gl.RenderPipelines;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.tooltip.Tooltip;
@@ -11,7 +12,6 @@ import net.minecraft.client.render.*;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
-import org.joml.Matrix4f;
 
 public final class CommandBlockTypeButton extends IconButton implements Dirtyable {
 	private CommandBlockBlockEntity.Type type = CommandBlockBlockEntity.Type.REDSTONE;
@@ -80,15 +80,11 @@ public final class CommandBlockTypeButton extends IconButton implements Dirtyabl
 	public void renderWidget(DrawContext context, int mouseX, int mouseY, float delta) {
 		var texture = getTexture();
 
-		RenderSystem.enableBlend();
-		RenderSystem.defaultBlendFunc();
-		RenderSystem.enableDepthTest();
-
 		int color = active ? 0xFFFFFFFF : 0x7FFFFFFF;
 		int shadowColor = 0x3F000000;
 
 		if (active) {
-			context.drawTexture(RenderLayer::getGuiTextured, texture, getX() + 1, getY() + 1, 0, 0, iconWidth, iconHeight, 16, 64, shadowColor);
+			context.drawTexture(RenderPipelines.GUI_TEXTURED, texture, getX() + 1, getY() + 1, 0, 0, iconWidth, iconHeight, 16, 64, shadowColor);
 		}
 
 		// To flip the texture, we pass u2, u1, v2, v1 instead of the usual
@@ -96,10 +92,7 @@ public final class CommandBlockTypeButton extends IconButton implements Dirtyabl
 		// instead of going through context.drawTexture.
 		int x1 = getX(), x2 = x1 + 16, y1 = getY(), y2 = y1 + 16;
 		float u1 = 0.0f, u2 = 1.0f, v1 = 0.0f, v2 = 0.25f;
-		((DrawContextAccessor)context).invokeDrawTexturedQuad(RenderLayer::getGuiTextured, texture, x1, x2, y1, y2, u2, u1, v2, v1, color);
-
-		RenderSystem.disableDepthTest();
-		RenderSystem.disableBlend();
+		((DrawContextAccessor)context).invokeDrawTexturedQuad(RenderPipelines.GUI_TEXTURED, texture, x1, x2, y1, y2, u2, u1, v2, v1, color);
 	}
 
 	@Override
