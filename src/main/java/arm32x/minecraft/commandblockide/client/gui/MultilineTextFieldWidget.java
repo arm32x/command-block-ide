781d55e99ee8483e4cba4fb0d5cdcbb0fa04617b
arm32x <github.account@arm32.ax>
2026-02-07 22:59:05 -0500

    Fix selection color

--- src/main/java/arm32x/minecraft/commandblockide/client/gui/MultilineTextFieldWidget.java
+++ src/main/java/arm32x/minecraft/commandblockide/client/gui/MultilineTextFieldWidget.java
@@ -332,15 +332,15 @@ public class MultilineTextFieldWidget extends TextFieldWidget {
 
         if (startY == endY) {
             // Selection spans one line
-            context.drawSelection(startX, startY, endX, endY + lineHeight - 1, false);
+            context.drawSelection(startX, startY, endX, endY + lineHeight - 1, true);
         } else {
             // Selection spans two or more lines
-            context.drawSelection(startX, startY, rightEdge, startY + lineHeight, false);
+            context.drawSelection(startX, startY, rightEdge, startY + lineHeight, true);
             if (!(startY - lineHeight == endY || endY - lineHeight == startY)) {
                 // Selection spans three or more lines
-                context.drawSelection(leftEdge, startY + lineHeight, rightEdge, endY, false);
+                context.drawSelection(leftEdge, startY + lineHeight, rightEdge, endY, true);
             }
-            context.drawSelection(leftEdge, endY, endX, endY + lineHeight - 1, false);
+            context.drawSelection(leftEdge, endY, endX, endY + lineHeight - 1, true);
         }
 	}
 
